e2db98f789e35041f1f36116d253aa0b6210aee6
JSHS-PLMA <plma@jshsus.kr>
2025-05-13 09:44:18 +0900

    errorfix3

--- server/views.py
+++ server/views.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import tempfile
 from yt_dlp import YoutubeDL
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from django.http import JsonResponse, HttpResponseServerError
@@ -16,33 +17,36 @@ s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
 
 def download_and_upload_to_s3(video_url):
     try:
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': '%(title)s.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'cookiefile': '/home/ubuntu/Server/plma_python/server/cookies.txt'
-        }
+        # 임시 디렉토리 생성
+        with tempfile.TemporaryDirectory() as tmpdir:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': os.path.join(tmpdir, '%(title)s.%(ext)s'),
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'cookiefile': '/home/ubuntu/Server/plma_python/server/cookies.txt'
+            }
 
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(video_url, download=True)
-            filename = ydl.prepare_filename(info).replace(info['ext'], 'mp3')
-            print(f"Downloaded: {filename}")
+            with YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(video_url, download=True)
+                file_path = os.path.join(tmpdir, f"{info['title']}.mp3")
+                print(f"Downloaded temp file: {file_path}")
 
-        return upload_to_s3(filename)
+            # S3로 바로 업로드
+            return upload_fileobj_to_s3(file_path, info['title'] + '.mp3')
 
     except Exception as e:
         print(f"YT-DLP Error: {str(e)}")
         return HttpResponseServerError(f"YT-DLP Error: {str(e)}")
 
-def upload_to_s3(file_name):
+def upload_fileobj_to_s3(file_path, s3_filename):
     try:
-        with open(file_name, 'rb') as file_data:
-            s3_key = f"songs/{file_name}"
-            s3_client.put_object(Bucket=AWS_STORAGE_BUCKET_NAME, Key=s3_key, Body=file_data, ACL='public-read')
+        with open(file_path, 'rb') as file_data:
+            s3_key = f"songs/{s3_filename}"
+            s3_client.upload_fileobj(file_data, AWS_STORAGE_BUCKET_NAME, s3_key, ExtraArgs={'ACL': 'public-read'})
 
         print(f"Uploaded to S3: {s3_key}")
         s3_link = f"https://s3-{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{s3_key}"
