aa58a9c90b84f83b72fe29851f24aa97534e6125
JSHS-PLMA <plma@jshsus.kr>
2025-05-14 13:40:36 +0900

    ds

--- server/views.py
+++ server/views.py
@@ -7,21 +7,18 @@ def youtube_audio(request):
     video_id = request.GET.get('videoId')
     if not video_id:
         return HttpResponse("videoId 파라미터가 필요합니다.", status=400)
-        
-    print("MEDIA_ROOT:", settings.MEDIA_ROOT)
-    print("다운로드 경로:", os.path.join(settings.MEDIA_ROOT, f"{video_id}.%(ext)s"))
 
-    filename = f"{video_id}.mp3"  # ✅ 확장자 포함!
+    filename = f"{video_id}.mp3"
     download_path = os.path.join(settings.MEDIA_ROOT, filename)
 
-    # media 폴더 없으면 생성
     if not os.path.exists(settings.MEDIA_ROOT):
         os.makedirs(settings.MEDIA_ROOT)
 
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': os.path.join(settings.MEDIA_ROOT, f"{video_id}.%(ext)s"),  # ✅ 확장자 자동 대체
-        'ffmpeg_location': '/usr/bin/ffmpeg',
+        'outtmpl': os.path.join(settings.MEDIA_ROOT, f"{video_id}.%(ext)s"),
+        'ffmpeg_location': '/usr/bin/ffmpeg', # C:/src/ffmpeg/bin | /usr/bin/ffmpeg
+        'cookies': './cookies.txt',  # ✅ 로그인 인증을 위한 쿠키 파일 경로 추가
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
